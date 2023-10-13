43091375bcb64e6d9c62cbafb783b1554726c5be
Rohith441438 <rohithvinay889@gmail.com>
2023-10-13 10:32:13 +0530

    Add project files.

---
+++ WebApp/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("Admin")]
+        [Authorize(Roles ="Admin")]
+        public IActionResult AdminEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"Hi {currentUser.Givenname}, you are an {currentUser.Role}");
+        }
+
+        [HttpGet("Seller")]
+        [Authorize(Roles = "Seller")]
+        public IActionResult SellerEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"Hi {currentUser.Givenname}, you are a {currentUser.Role}");
+        }
+
+        [HttpGet("AdminAndSeller")]
+        [Authorize(Roles = "Admin, Seller")]
+        public IActionResult AdminAndSellerEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"Hi {currentUser.Givenname}, you are a {currentUser.Role}");
+        }
+
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if(identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new UserModel
+                {
+                    Username = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value,
+                    EmailAddress = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+                    Givenname = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.GivenName)?.Value,
+                    Surname = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Surname)?.Value,
+                    Role = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }
+    }
+}
---
+++ WebApp/Models/UserConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class UserConstants
+    {
+        public static IEnumerable<UserModel> users = new List<UserModel>
+        {
+            new UserModel{ Username = "rohith_admin", Password = "R_H_i_t_H", EmailAddress="rohithemail.gmail.com", Role="Admin", Surname="Govindalapudi", Givenname="RohithVinay"},
+            new UserModel{ Username = "praharshin_seller", Password = "P_a_H_r_H_n_", EmailAddress="Praharshiniemail.gmail.com", Role="Seller", Surname="Tangella", Givenname="Praharshini"}
+        };
+    }
+}
